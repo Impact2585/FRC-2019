a804e544da95d6efbe7acb1f2a539e0d2bc669de
Ryan Knightly <reknightly@gmail.com>
2019-02-06 21:57:36 -0600

    Create initial project

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Add your docs here.
+ */
+public class ExampleTest{
+    @Test
+	public void examplePassingTest() {
+		assertTrue(true);
+    }
+}
