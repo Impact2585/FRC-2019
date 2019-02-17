9b604f7fff31f59781e49c975c8ab0e87d00b443
jxin31415 <jxin31415@gmail.com>
2019-02-17 11:24:28 -0600

    Add Revrobotics Dependency

--- src/main/java/frc/systems/ElevatorSystem.java
+++ src/main/java/frc/systems/ElevatorSystem.java
@@ -7,7 +7,9 @@
 
 package frc.systems;
 
-import edu.wpi.first.wpilibj.Spark;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import frc.input.InputMethod;
 import frc.robot.RobotMap;
 
@@ -17,8 +19,8 @@ import frc.robot.RobotMap;
 public class ElevatorSystem extends RobotSystem {
   private final double LIFT_SPEED = 0.5;
   
-  private Spark elevatorMotor;
-
+  //private Spark elevatorMotor;
+  private CANSparkMax elevatorMotor;
 
   /**
    * Creates a new elevatorSystem
@@ -31,7 +33,8 @@ public class ElevatorSystem extends RobotSystem {
 
   @Override
   public void init() {
-    elevatorMotor = new Spark(RobotMap.ELEVATOR_MOTOR);
+    elevatorMotor = new CANSparkMax(RobotMap.ELEVATOR_MOTOR, MotorType.kBrushless);
+    elevatorMotor.restoreFactoryDefaults();
   }
 
   @Override
@@ -56,7 +59,7 @@ public class ElevatorSystem extends RobotSystem {
    * Sets the speed of the lift motor
    */
   protected void setLiftSpeed(double speed) {
-    elevatorMotor.setSpeed(speed);
+    elevatorMotor.set(speed);
   }
 
 }
