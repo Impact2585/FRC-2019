44ce2ac2c5a77b034d98a5f46912b135729e766f
jxin31415 <jxin31415@gmail.com>
2019-03-15 09:01:19 -0500

    Dual Controller

--- src/main/java/frc/input/XBoxInput.java
+++ src/main/java/frc/input/XBoxInput.java
@@ -15,54 +15,55 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
  */
 public class XBoxInput extends InputMethod {
   private XboxController controller;
+  private XboxController controller2;
   private final double JOYSTICK_DEAD_ZONE = 0.05;
   private final double TRIGGER_DEAD_ZONE = 0.1;
 
   public XBoxInput() {
     // 0 is the port # of the driver station the joystick is plugged into
     controller = new XboxController(0);
+    controller2 = new XboxController(1);
   }
 
   @Override
   public double forwardAmount() {
-    double forward = controller.getY(Hand.kLeft);
+    double forward = (Math.abs(controller.getY(Hand.kLeft)) > Math.abs(controller2.getY(Hand.kLeft))) ? controller.getY(Hand.kLeft) : controller2.getY(Hand.kLeft);
     return (Math.abs(forward) < JOYSTICK_DEAD_ZONE) ? 0 : forward;
   }
 
   @Override
   public double turnAmount() {
-    double turn = controller.getX(Hand.kRight);
-
+    double turn = (Math.abs(controller.getX(Hand.kRight)) > Math.abs(controller2.getX(Hand.kRight))) ? controller.getX(Hand.kRight) : controller2.getX(Hand.kRight);
     return (Math.abs(turn) < JOYSTICK_DEAD_ZONE) ? 0 : turn;
   }
 
   @Override
   public boolean shouldPivotUp() {
-    return controller.getYButton();
+    return controller.getYButton() || controller2.getYButton();
   }
   
   @Override
   public boolean shouldPivotDown() {
-    return controller.getXButton();
+    return controller.getXButton() || controller2.getXButton();
   }
   
   @Override
   public boolean shouldIntake() {
-    return controller.getTriggerAxis(Hand.kLeft) > TRIGGER_DEAD_ZONE;
+    return Math.max(controller.getTriggerAxis(Hand.kLeft), controller2.getTriggerAxis(Hand.kLeft)) > TRIGGER_DEAD_ZONE;
   }
   
   @Override
   public boolean shouldOuttake() {
-    return controller.getTriggerAxis(Hand.kRight) > TRIGGER_DEAD_ZONE;
+    return Math.max(controller2.getTriggerAxis(Hand.kLeft), controller.getTriggerAxis(Hand.kRight)) > TRIGGER_DEAD_ZONE;
   }
 
   @Override
   public boolean shouldLiftElevator(){
-    return controller.getAButton();
+    return controller.getAButton() || controller2.getAButton();
   }
 
   @Override
   public boolean shouldLowerElevator(){
-    return controller.getBButton();
+    return controller.getBButton() || controller2.getBButton();
   }
 }
